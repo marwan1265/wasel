31431b6c6c941dd99014f1f653634fd7502ba003
marwan1265 <marwan13223@gmail.com>
2025-05-19 00:00:29 +0300

    fixed conversation id generation using uuid

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Chat } from '@/components/chat'
 import { getModels } from '@/lib/config/models'
-import { generateId } from 'ai'
+// import { generateId } from 'ai'
+import { v4 as uuidv4 } from 'uuid'
 
 export default async function Page() {
-  const id = generateId()
+  // const id = generateId()
+  const id = uuidv4()
   const models = await getModels()
   return <Chat id={id} models={models} />
 }
