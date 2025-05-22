e679374897c1f52d9e9a854aa8f9be45acb55564
marwan1265 <marwan13223@gmail.com>
2025-05-23 03:04:01 +0400

    removed the new-chat-button.tsx to simplify the new chat logic

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -3,15 +3,16 @@ import {
     SidebarContent,
     SidebarHeader,
     SidebarMenu,
+    SidebarMenuButton,
     SidebarMenuItem,
     SidebarRail
 } from '@/components/ui/sidebar'
 import { cn } from '@/lib/utils'
+import { Plus } from 'lucide-react'
 import Link from 'next/link'
 import { Suspense } from 'react'
 import { ChatHistorySection } from './sidebar/chat-history-section'
 import { ChatHistorySkeleton } from './sidebar/chat-history-skeleton'
-import { NewChatButton } from './sidebar/new-chat-button'
 import { IconLogo } from './ui/icons'
 import SidebarTrigger from './ui/sidebar-trigger'
 
@@ -28,7 +29,12 @@ export default function AppSidebar() {
       <SidebarContent className="flex flex-col px-2 py-4 h-full">
         <SidebarMenu>
           <SidebarMenuItem>
-            <NewChatButton />
+            <SidebarMenuButton asChild>
+              <Link href="/" className="flex items-center gap-2">
+                <Plus className="size-4" />
+                <span>محادثة جديدة</span>
+              </Link>
+            </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
         <div className="flex-1 overflow-y-auto">
